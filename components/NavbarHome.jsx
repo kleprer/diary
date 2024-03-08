92bb458493f3405ceaac2a32e0cfe71377c6d6c9
Maximova Snezhana <kleprer@gmail.com>
2024-03-08 23:37:49 +0300

    styling войти on hover

--- components/NavbarHome.jsx
+++ components/NavbarHome.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 const NavbarHome = () => {
   return (
     <nav className="flex justify-end items-center w-full h-72 bg-white">
-        <div className="border-black border-4 rounded-xl mr-14">
-            <h2 className="text-{20} text-black px-8 py-2">Войти</h2>
-        </div>
+            <h2 className=" mr-14 text-{20} text-black px-8 py-3 border-black border-4 rounded-xl hover:border-gray-300 hover:text-gray-300 hover:cursor-pointer">
+                Войти
+            </h2>
     </nav>
   )
 }
