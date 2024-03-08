fab1366130a95aac8075af8d070938bb989131e9
Maximova Snezhana <kleprer@gmail.com>
2024-03-09 00:54:53 +0300

    adding image on home

--- app/page.js
+++ app/page.js
@@ -1,10 +1,13 @@
 import NavbarController from "@/components/NavbarController";
-
+import Image from "next/image";
 export default function Home() {
   return (
     <>
       <NavbarController type={1} />
-      
+      <Image className="position: absolute top-0 h-full w-auto" 
+      src="/Mask group.png" 
+      alt="хомяк" width={1162} height={1000} 
+      priority={true}/>
     </>
 
   );
