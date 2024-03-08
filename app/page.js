cd95c18c0d5b23168f01f65ef112c60f9a135317
Maximova Snezhana <kleprer@gmail.com>
2024-03-09 01:26:22 +0300

    adding description on landing page

--- app/page.js
+++ app/page.js
@@ -1,13 +1,16 @@
+import Description from "@/components/Description";
 import NavbarController from "@/components/NavbarController";
 import Image from "next/image";
 export default function Home() {
   return (
     <>
       <NavbarController type={1} />
+      <Description />
       <Image className="position: absolute top-0 h-full w-auto" 
       src="/Mask group.png" 
       alt="хомяк" width={1162} height={1000} 
       priority={true}/>
+      
     </>
 
   );
---
+++ components/Description.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Description = () => {
+  return (
+    <div className="flex flex-col gap-[21px] position: absolute w-[750px] h-[265px] top-[200px] right-[0px]">
+        <h1 className="text-[40px] m-0 p-0">Добро пожаловать в ваш персональный ежедневник!</h1>
+        <h2 className="text-[20px] m-0 p-0">Планируйте и создавайте заметки прямо здесь</h2>
+    </div>
+  )
+}
+
+export default Description
