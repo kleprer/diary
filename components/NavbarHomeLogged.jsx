88d442be61c23ec169832e031eb3293b258186ca
Maximova Snezhana <kleprer@gmail.com>
2024-03-21 00:32:30 +0300

    adding NavbarHomeLogged component for the profile icon in the nav home when logged in

--- components/NavbarHomeLogged.jsx
+++ components/NavbarHomeLogged.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const NavbarHomeLogged = () => {
   return (
-    <div>NavbarHomeLogged</div>
+    <nav className="flex justify-end items-center w-full h-72 bg-transparent top-0 mt-2 position: relative">
+            <h2 className="text-center mr-[20px] align-top text-black px-[13px] py-[13px] border-black border-4 rounded-full 
+            hover:border-gray-300 hover:text-gray-300 hover:cursor-pointer
+            w-[52px] h-[52px]">
+                A
+            </h2>
+    </nav>
   )
 }
 
