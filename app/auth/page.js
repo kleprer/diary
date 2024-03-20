6434ffee62ad1e4dbeb02e51fea4a7c0d1f94157
Maximova Snezhana <kleprer@gmail.com>
2024-03-21 00:52:49 +0300

    adding authwindow component to /auth

--- app/auth/page.js
+++ app/auth/page.js
@@ -1,9 +1,11 @@
+import AuthWindow from "@/components/AuthWindow";
 import NavbarAuth from "@/components/NavbarAuth";
 
 export default function Auth() {
     return (
         <>
         <NavbarAuth />
+        <AuthWindow />
         </>
     )
 }
--- components/AuthWindow.jsx
+++ components/AuthWindow.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const AuthWindow = () => {
   return (
-    <>
-    
-    </>
+    <div className="w-[710px] h-[489px] border-black border-4 rounded-xl m-auto mt-[20px]">
+        
+    </div>
   )
 }
 
