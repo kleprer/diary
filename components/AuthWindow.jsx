0767f77bb5f4fd88f7296df57619fe5a7fd6a869
Maximova Snezhana <kleprer@gmail.com>
2024-03-21 01:20:12 +0300

    not pixel perfect-ly stylized authwindow

--- components/AuthWindow.jsx
+++ components/AuthWindow.jsx
@@ -3,7 +3,30 @@ import React from 'react'
 const AuthWindow = () => {
   return (
     <div className="w-[710px] h-[489px] border-black border-4 rounded-xl m-auto mt-[20px]">
-        
+        <div className="flex align-center flex-col w-[554px] m-auto">
+            <h2 className="text-[32px] text-center pt-[30px]">
+                С возвращением!
+            </h2>
+            <input className="text-[20px] text-black opacity-[60%] mt-[50px]
+            border-b-4 border-black" type="text" placeholder="Электронная почта">
+
+            </input>
+            <input className="text-[20px] text-black opacity-[60%] mt-[50px]
+            border-b-4 border-black" type="text" placeholder="Пароль">
+
+            </input>
+            <button className="m-auto text-[24px] text-center w-[238px] 
+            border-black border-4 rounded-xl mt-[35px] py-[6px]">
+                Войти
+            </button>
+            <h2 className="text-[20px] text-center mt-[15px]">
+                или
+            </h2>
+            <button className="m-auto text-[24px] text-center w-[238px] 
+            border-black border-4 rounded-xl mt-[15px] py-[6px]">
+                Регистрация
+            </button>
+        </div>
     </div>
   )
 }
