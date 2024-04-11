c8cb751b5d8274a16d121515b397887ed986eaea
Maximova Snezhana <kleprer@gmail.com>
2024-04-11 22:04:15 +0300

    imported custom styling for calendar

--- app/calendar/page.js
+++ app/calendar/page.js
@@ -3,6 +3,7 @@ import NavbarCalendar from "/components/NavbarCalendar";
 import Card from "/components/Card";
 import { useState } from "react";
 import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
 
 export default function CalendarTasksGoals() {
 
@@ -15,7 +16,11 @@ export default function CalendarTasksGoals() {
         <>
             <NavbarCalendar />
             <div className="flex w-full items-center gap-[30px] flex-row">
-                <Calendar locale="ru-RU" onChange={onChange} value={date} />
+                <div className="w-full h-full p-10">
+                    <Calendar locale="ru-RU" onChange={setDate} value={date} 
+                    className="
+                    border-4 border-black w-full h-full p-[15px] rounded-xl"/>
+                </div>
                 <div className="flex flex-col gap-[15px] items-end w-full 
                 pl-[10px] pr-[10px] pt-[10px] h-[288px]">
                     <Card title={"Планы на 03.03"} />
