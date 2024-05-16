b11955915801c7c73148362c55aaca3507d30145
Maximova Snezhana <kleprer@gmail.com>
2024-05-16 13:40:59 +0300

    adding textarea to diarywindow (not fully styled but it works so yeah)

--- components/DairyWindow.jsx
+++ components/DairyWindow.jsx
@@ -2,8 +2,12 @@ import React from "react";
 
 const DairyWindow = () => {
     return (
-        <div className="w-[1356px] h-[769px] border-black border-4 rounded-xl m-auto mt-[42px]">        
-            
+        <div className="h-[520px] mb-[20px] ml-[10px] mr-[20px] 
+        border-black border-[4px] rounded-xl mt-[20px]">        
+            <textarea className="peer w-full min-h-full  resize-none p-[10px]
+            outline-none focus:border-t-transparent  rounded-xl
+            focus:outline-0 disabled:resize-none disabled:border-0"
+            placeholder="Как прошел ваш день?"></textarea>
         </div>
     )
 }
