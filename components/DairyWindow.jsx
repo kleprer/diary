47137109b95ef9fe2746e4c87663b9dc5876927c
Maximova Snezhana <kleprer@gmail.com>
2024-05-24 02:50:51 +0300

    fixed diary page styling

--- components/DairyWindow.jsx
+++ components/DairyWindow.jsx
@@ -2,7 +2,7 @@
 import React from "react";
 import { useState } from "react";
 import Calendar from 'react-calendar';
-import "/учеба/пп/diary/app/Calendar.css"
+import '../app/DiaryCalendar.css';
 
 const DairyWindow = () => {
     const [date, setDate] = useState(new Date());
@@ -11,20 +11,31 @@ const DairyWindow = () => {
     }
     const [isOpen, setIsOpen] = useState(false)
     return (
-        <div className="h-[520px] mb-[20px] ml-[10px] mr-[20px] 
-        border-black border-[4px] rounded-xl mt-[20px]">   
-            <h2 className=" flex flex-col w-[470px] h-[60px] ml-[20px] border-black border-b-[4px] text-[28px] pb-[15px]">{date.getDate()}.0{date.getMonth()+1}
-                <button className="h-[45px] w-[268px] ml-[200px] border-black border-4 rounded-xl text-[20px]" onClick={() => setIsOpen((prev) => !prev) }> Выбарать дату</button>
-                {isOpen && (
-                    <div className="w-full h-full ml-[190px] pl-[10px] pt-[10px] relative">
-                    <Calendar locale="ru-RU" onChange={setDate} value={date} 
-                    className="border-4 border-black w-full m-0  
-                    p-[15px] rounded-xl"/>
-                </div>
-                )}
-            </h2>
-            <textarea className="static mt-[20px] ml-[20px] peer w-[1850px] h-[400px] resize-none p-[10px]
-            outline-none focus:border-t-transparent  rounded-xl
+        <div className="h-[520px] mb-[10px] ml-[10px] mr-[20px] 
+        border-black border-[4px] rounded-xl mt-[20px] p-[3px]"> 
+            <div className="flex items-end mt-[10px] gap-[20px]">  
+                <h2 className="flex flex-col w-[250px] h-[60px] pt-[7px] ml-[20px] 
+                border-black border-b-[4px] text-[26px] ">
+                {date.getMonth()+1 < 10 
+                            ? `${date.getDate()}.0${date.getMonth()+1}.${date.getFullYear()}`
+                            : `${date.getDate()}.${date.getMonth()+1}.${date.getFullYear()}`
+                        }
+                    {isOpen && (
+                        <div className="w-[400px] h-full ml-[260px] pl-[10px] mt-[10px] pt-[10px] relative">
+                            <Calendar locale="ru-RU" onChange={setDate} value={date} 
+                            className="border-4 border-black w-full m-0  
+                            p-[15px] rounded-xl"/>
+                        </div>
+                    )}
+                </h2>
+                <button className="h-[55px] w-[235px] border-black border-4 rounded-xl 
+                text-[20px]" 
+                    onClick={() => setIsOpen((prev) => !prev) }>
+                        Выбрать дату
+                </button>
+            </div>
+            <textarea className="static mt-[20px] peer w-full h-[400px] resize-none p-[15px]
+            outline-none focus:border-t-transparent  rounded-xl text-[20px]
             focus:outline-0 disabled:resize-none disabled:border-0"
             placeholder="Как прошел ваш день?"></textarea>
         </div>
