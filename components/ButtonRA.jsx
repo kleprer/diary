84019d36abfc71b56cf721e7f9d40908394fa3ee
Maximova Snezhana <kleprer@gmail.com>
2024-05-28 03:07:41 +0300

    readable code

--- components/ButtonRA.jsx
+++ components/ButtonRA.jsx
@@ -3,8 +3,7 @@ import React from "react";
 const ButtonRA = (props) => {
     return(
         <div>
-            <button className="text-[24px] text-center w-[296px] 
-            border-black border-4 rounded-xl mt-[15px] py-[6px]
+            <button className="text-[24px] text-center w-[296px] border-black border-4 rounded-xl mt-[15px] py-[6px]
             hover:border-gray-300 hover:text-gray-300">
                 {props.name}
             </button>
